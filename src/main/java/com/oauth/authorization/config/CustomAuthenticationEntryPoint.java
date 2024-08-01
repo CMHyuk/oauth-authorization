f6d86aaf65220f3530d7155c3541f03f6b923dd5
choi min hyuk <minhyeok.choi@softcamp.co.kr>
2024-08-01 17:32:56 +0900

    feat: 커스텀 엔트리 포인트 설정

---
+++ src/main/java/com/oauth/authorization/config/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,20 @@
+package com.oauth.authorization.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        String redirectUri = URLEncoder.encode("login", UTF_8);
+        response.sendRedirect(redirectUri);
+    }
+}
--- src/main/java/com/oauth/authorization/config/SecurityConfig.java
+++ src/main/java/com/oauth/authorization/config/SecurityConfig.java
@@ -14,9 +14,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(request ->
-                request.requestMatchers("/login").permitAll()
-                        .anyRequest().authenticated()
-        );
+                        request.requestMatchers("/login", "/oauth2/authorize", "/oauth2/token").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .exceptionHandling(exceptionHandling ->
+                        exceptionHandling.authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                );
         http.formLogin(AbstractHttpConfigurer::disable);
         return http.build();
     }
