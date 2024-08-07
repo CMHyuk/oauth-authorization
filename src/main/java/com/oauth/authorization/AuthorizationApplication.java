c2fc3dfbda90271fd7a5a845fd8b6c7121369805
choi min hyuk <minhyeok.choi@softcamp.co.kr>
2024-08-07 10:55:48 +0900

    style: 사용하지 않는 import 제거

--- src/main/java/com/oauth/authorization/AuthorizationApplication.java
+++ src/main/java/com/oauth/authorization/AuthorizationApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, ErrorMvcAutoConfiguration.class})
