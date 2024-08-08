f7ee50266de404dccb6404f56034ec66e6a9c421
choi min hyuk <minhyeok.choi@softcamp.co.kr>
2024-08-08 10:33:16 +0900

    feat: JwtGenerator 대신 CustomJwtGenerator 등록

--- src/main/java/com/oauth/authorization/config/SecurityConfig.java
+++ src/main/java/com/oauth/authorization/config/SecurityConfig.java
@@ -1,10 +1,11 @@
 package com.oauth.authorization.config;
 
+import com.oauth.authorization.domain.token.service.CustomJwtGenerator;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -16,26 +17,29 @@ import org.springframework.security.oauth2.server.authorization.settings.Authori
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
+@Configuration
 @EnableWebSecurity
-@Configuration(proxyBeanMethods = false)
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomJwtGenerator customJwtGenerator;
 
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
     public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfigurer authorizationServerConfigurer = new OAuth2AuthorizationServerConfigurer();
+        authorizationServerConfigurer.setBuilder(http);
+        authorizationServerConfigurer.tokenGenerator(customJwtGenerator);
+
         OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
-        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class).oidc(Customizer.withDefaults());
-        http
-                .exceptionHandling(exceptions ->
-                        exceptions.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
-                );
+        http.exceptionHandling(exceptions -> exceptions.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login")));
+
         return http.build();
     }
 
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorizeRequests ->
+        http.authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 .requestMatchers("/login").permitAll()
                                 .anyRequest().authenticated()
