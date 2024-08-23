a21d1b711b0c4699d1355906927d8be4023ed989
choi min hyuk <minhyeok.choi@softcamp.co.kr>
2024-08-23 09:34:51 +0900

    chore: kubernetes deploy

--- jenkins-pipelines.groovy
+++ jenkins-pipelines.groovy
@@ -6,7 +6,6 @@ podTemplate(
                 containerTemplate(name: 'maven', image: 'maven:3.8.3-openjdk-17', command: "cat", ttyEnabled: true),
                 containerTemplate(name: "docker", image: "docker", command: "cat", ttyEnabled: true)
         ],
-        // 현재 파드 내부에서 docker를 사용하기 위한 볼륨 마운트
         volumes: [
                 hostPathVolume(hostPath: "/var/run/docker.sock", mountPath: "/var/run/docker.sock"),
                 persistentVolumeClaim(mountPath: '/root/.m2/repository', claimName: 'maven-repo-storage', readOnly: false)
@@ -33,6 +32,26 @@ podTemplate(
                                 dockerApp.push("latest")
                             }
                         }
+                    }
+                }
+
+                // 기존에 배포되어 동작중이던 deployment를 삭제한다.
+                try{
+                    stage('Kubernetes destroy exist pod & service') {
+                        container("kubectl") {
+                            sh "kubectl delete deployments authorization-minhyeok -n jenkins"
+                            sh "kubectl delete service authorization-minhyeok-service -n jenkins"
+                        }
+                    }
+                } catch(e) {
+                    sh "echo Does not exist kubernetes pod, service"
+                }
+
+                // 빌드된 docker container를 사용하는 deployment를 배포를 한다.
+                stage('Kubernetes deployment pod') {
+                    container("kubectl") {
+                        sh "sed -i 's/BUILD_NUMBER/${VERSION}/g' jenkins-deploy.yml"
+                        sh "kubectl apply -f jenkins-deploy.yml"
                     }
                 }
             }
