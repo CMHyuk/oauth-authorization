3f33483f7751081a6a5d490730f1189cfa48b782
choi min hyuk <minhyeok.choi@softcamp.co.kr>
2024-08-28 13:56:05 +0900

    chore: Kubernetes Deploy

--- jenkins-pipelines.groovy
+++ jenkins-pipelines.groovy
@@ -20,31 +20,33 @@ podTemplate(
                     checkout scm
                 }
 
-                stage('Build') {
-                    // pom.xml을 이용한 라이브러리 정보 추출
-                    VERSION = readMavenPom().getVersion()
+//                stage('Build') {
+//                    // pom.xml을 이용한 라이브러리 정보 추출
+//                    VERSION = readMavenPom().getVersion()
+//
+//                    MAVEN_BUILD_OPT = "-Pdev clean verify package"
+//                    MAVEN_BUILD_SKIP_JUNIT_TEST = "-Dmaven.test.skip=true"
+//                    MAVEN_BUILD_DCHECK_OPT = "-Ddependency-check.skip=true"
+//
+//                    echo "Start BUILD ${PROJECT_NAME}:${VERSION} Build_Opt [${MAVEN_BUILD_OPT} ${MAVEN_BUILD_DCHECK_OPT} ${MAVEN_BUILD_SKIP_JUNIT_TEST}]"
+//
+//                    // Jenkins의 설정에서 configfile을 다운로드(기 생성된 정보를 이용한 Maven settings.xml을 다운로드)
+//                    configFileProvider([configFile(fileId: '0d84db68-876c-4c67-a8bd-c39b36969427', targetLocation: 'settings.xml', variable: 'AZURE_SETTINGS')]) {
+//                    }
+//
+//                    // maven docker 이미지에서 동작 시작
+//                    container('maven') {
+//                        // junit test에서 사용하는 cloud-oauth-service의 docker image를 다운로드 하기 위한 scr.softcamp.co.kr의 credential 정보 추가
+//                        configFileProvider([configFile(fileId: '78c72fcb-fab2-4d25-a223-efe8f15c93ff', variable: 'DOCKER_AUTH_CONFIG')]) {
+//                            sh """
+//                                mkdir -p /root/.docker
+//                                cat $DOCKER_AUTH_CONFIG > /root/.docker/config.json
+//                            """
+//                        }
+//                    }
+//                }
 
-                    MAVEN_BUILD_OPT = "-Pdev clean verify package"
-                    MAVEN_BUILD_SKIP_JUNIT_TEST = "-Dmaven.test.skip=true"
-                    MAVEN_BUILD_DCHECK_OPT = "-Ddependency-check.skip=true"
-
-                    echo "Start BUILD ${PROJECT_NAME}:${VERSION} Build_Opt [${MAVEN_BUILD_OPT} ${MAVEN_BUILD_DCHECK_OPT} ${MAVEN_BUILD_SKIP_JUNIT_TEST}]"
-
-                    // Jenkins의 설정에서 configfile을 다운로드(기 생성된 정보를 이용한 Maven settings.xml을 다운로드)
-                    configFileProvider([configFile(fileId: '0d84db68-876c-4c67-a8bd-c39b36969427', targetLocation: 'settings.xml', variable: 'AZURE_SETTINGS')]) {
-                    }
-
-                    // maven docker 이미지에서 동작 시작
-                    container('maven') {
-                        // junit test에서 사용하는 cloud-oauth-service의 docker image를 다운로드 하기 위한 scr.softcamp.co.kr의 credential 정보 추가
-                        configFileProvider([configFile(fileId: '78c72fcb-fab2-4d25-a223-efe8f15c93ff', variable: 'DOCKER_AUTH_CONFIG')]) {
-                            sh """
-                                mkdir -p /root/.docker
-                                cat $DOCKER_AUTH_CONFIG > /root/.docker/config.json
-                            """
-                        }
-                    }
-                }
+                VERSION = readMavenPom().getVersion()
 
                 stage('Docker Build & Push') {
                     container("docker") {
@@ -57,6 +59,7 @@ podTemplate(
                 }
 
                 stage('Kubernetes Deploy') {
+
                     container("kubectl") {
                         CURRENT_VERSION = sh(script: "kubectl describe deployment ${PROJECT_NAME} -n ${KUBE_NAMESPACE} | grep Image: | awk -F ':' '{ print \$3 }'",
                                 returnStdout: true).trim()
